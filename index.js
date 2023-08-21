6dab86fe4415d793a3e8f47c9b49d4bfb1c21386
KliksBCA <142782908+KliksBCA@users.noreply.github.com>
2023-08-21 22:26:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const namasaya = "kongali1720";
+
+console.log(namasaya);
+
+function getname() {
+    for (let i= 0; i < 1000; i++) {
+        console.log('nama saya adalah ' namasaya)
+    }
+}
+
+getname();
+
+
